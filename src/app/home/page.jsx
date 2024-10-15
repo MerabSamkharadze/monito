d53cd0d62955098e409b90756d12e1309e5df666
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-15 14:56:34 +0400

    bunner for homepage is done!

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,17 +1,35 @@
 import Image from "next/image";
 import banner from "@/public/images/banner.jpg";
 import Heading from "@/components/Heading";
+import Button from "@/components/Button";
 
 export default function page() {
   return (
     <>
       <section>
+        <div className="absolute mt-52 ml-32 z-10 max-w-lg ">
+          <h2 className=" font-extrabold text-6xl text-primary">
+            One more friend
+          </h2>
+          <h3 className=" font-bold mt-5  text-5xl text-primary ">
+            Thousands more fun!
+          </h3>
+          <p className="mt-10">
+            Having a pet means you have more joy, a new friend, a happy person
+            who will always be with you to have fun. We have 200+ different pets
+            that can meet your needs!
+          </p>
+          <div className="flex items-center  gap-4  mt-9">
+            <Button content={"View Intro"} />
+            <Button content={"Explore Now"} fill />
+          </div>
+        </div>
         <Image
           src={banner}
           alt="banner"
           width={1440}
           height={695}
-          className="rounded-bl-[45px] rounded-br-[45px]"
+          className="rounded-bl-[45px] rounded-br-[45px] relative"
         />
       </section>
       <section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 
 export default function Header() {
   return (
-    <header className=" flex items-center justify-between py-8 px-32 w-full h-10 bg-transparent absolute z-50">
+    <header className=" flex items-center justify-between py-16 px-32 w-full h-10 bg-transparent absolute top-0 left-0 z-50">
       <nav className="flex justify-between items-center w-[28rem]">
         <Link href={"/"}>
           <Image
