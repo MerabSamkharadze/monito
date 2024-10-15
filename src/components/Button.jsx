a4bb1674b28af8e6527d5f5f961ea0901ac9d749
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-15 18:29:27 +0400

    add hover effect on button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 export default function Button({ content, fill }) {
   return fill ? (
-    <button className="h-11 text-white bg-primary  py-3 px-7 rounded-full">
+    <button className="h-11 text-white bg-primary py-3 px-7 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105 ">
       {content}
     </button>
   ) : (
-    <button className=" h-11   bg-transparent text-primary border borrder py-2 px-6 rounded-full border-primary">
+    <button className="h-11 bg-transparent text-primary border border-primary py-2 px-6 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-primary hover:text-white">
       {content}
     </button>
   );
